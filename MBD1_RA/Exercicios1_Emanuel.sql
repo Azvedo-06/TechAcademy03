1241741af859bb98f0a8acd54a444015f444f380
Emanuel Ramos Paiva <emanuelramospaiva@gmail.com>
2024-08-12 20:18:10 -0300

    Exercicio de banco de dados

---
+++ MBD1_RA/Exercicios1_Emanuel.sql
@@ -0,0 +1,50 @@
+/*EX 01*/
+select * from produto
+where UNIDADE = 'un';
+
+/*EX 02*/
+select * from produto
+where CODIGO_CLASSIFICACAO = '020' and QUANTIDADE > 20;
+
+/*EX 03*/
+select * from produto
+where DESCRICAO like 'BALA %' and QUANTIDADE >= 6;
+
+/*EX 04*/
+select * from produto
+where DESCRICAO like '%martelo%' and CODIGO_CLASSIFICACAO != 'material de construção';
+
+/*EX 05*/
+select * from produto
+where CODIGO_CLASSIFICACAO = '002'  and UNIDADE = 'CX' and QUANTIDADE < '5'; 
+
+/*EX 06*/
+select * from produto
+where QUANTIDADE >= '10' and QUANTIDADE <= '50' and UNIDADE != 'CX' and CODIGO_CLASSIFICACAO = '002'; 
+
+/*EX 07*/
+select * from produto
+where DESCRICAO like'%Camiseta%' and CODIGO_CLASSIFICACAO = '021' or DESCRICAO like '%bola%' and CODIGO_CLASSIFICACAO = '008'
+
+/*EX 08*/
+select QUANTIDADE from produto 
+where CODIGO_CLASSIFICACAO = '003' and UNIDADE = 'PCT'
+
+/*EX 09*/
+select distinct p.UNIDADE
+from produto p 
+inner join classificacao c 
+on p.CODIGO_CLASSIFICACAO = c.CODIGO 
+where c.DESCRICAO = 'Veterinária';
+
+/*EX 10*/
+select DESCRICAO, QUANTIDADE from produto
+where QUANTIDADE > '6' and QUANTIDADE < '10' 
+
+/*EX 11*/
+select * from produto
+where DESCRICAO like 'oleo%' and UNIDADE = 'L'
+
+/*EX 12*/
+select *, (QUANTIDADE * VALOR) as TOTAL from produto
+where CODIGO_CLASSIFICACAO = '015' and UNIDADE = 'CX';
