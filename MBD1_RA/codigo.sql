75b5c28f943d626d0752826d920acd98dc14e2c7
Azvedo-06 <azinho006@gmail.com>
2024-08-27 21:27:34 -0300

    criando novo banco de dados

--- MBD1_RA/codigo.sql
+++ MBD1_RA/codigo.sql
@@ -1,24 +1,17 @@
-CREATE DATABASE ANDRE;
+create database game;
 
-CREATE TABLE `cenas` (
- `id_Cenas` Int(11) NOT NULL,
- `titulo` Varchar(300) DEFAULT NULL,
- `descricao` Varchar(300) DEFAULT NULL,
- `objetos` Varchar(100) DEFAULT NULL,
- PRIMARY KEY (`id_Cenas`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+create table game.cenas (
+	id_cena int not null primary key,
+	descricao varchar(260)
+);
 
-CREATE TABLE `objetos` (
- `id_Objetos` Int(11) NOT NULL,
- `obj_Nome` Varchar(100) DEFAULT NULL,
- `obj_Descricao` Varchar(300) DEFAULT NULL,
- `res_Negativo` Varchar(300) DEFAULT NULL,
- `res_Positiva` Varchar(300) DEFAULT NULL,
- `possivel_Leva` Tinyint(1) DEFAULT NULL,
- `comand_Positivo` Varchar(300) DEFAULT NULL,
- `pegar` Tinyint(1) DEFAULT NULL,
- `id_Cenas` Int(11) DEFAULT NULL,
- PRIMARY KEY (`id_Objetos`),
- KEY `id_Cenas` (`id_Cenas`),
- CONSTRAINT `objetos_ibfk_1` FOREIGN KEY (`id_Cenas`) REFERENCES `cenas` (`id_Cenas`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+create table game.itens (
+	id_item int not null primary key,
+	nome varchar(260),
+	desricao_positiva varchar(250),
+	desricao_negativa varchar(250),
+	comndo_correto varchar(255),
+	id_cena_atual int,
+	id_cena_destino int,
+	foreign key (id_cena_atual) references cenas(id_cena)
+);
