2f57bee84cea8c6103b58de8910f7bca9110582b
Azvedo-06 <azinho006@gmail.com>
2024-09-02 22:14:07 -0300

    Descrição da cena e nome do item da cena mandado para web com Spark e Gson.

--- EstruturaDados/src/Main.java
+++ EstruturaDados/src/Main.java
@@ -1,27 +1,37 @@
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+import com.google.gson.Gson;
 import model.Cena;
 import model.Item;
 import repository.CenaDAO;
 import repository.ItemDAO;
+import spark.Spark;
 
 public class Main {
     public Main() {
     }
 
-    public static void main(String[] args) {
-        try {
-            Cena cena = CenaDAO.findCenaById(1);
-            System.out.println(cena.toString());
+    private static final Gson GSON = new Gson();
 
-            for ( Item item : ItemDAO.findItemByScena(cena)) {
-                System.out.println("itens: " + item.getNome());
-            }
+    public static void main(String[] args) {
+
+       try {
+            Cena cena = CenaDAO.findCenaById(2);
+            List<Item> item = ItemDAO.findItemByScena(cena);
+
+           List<String> cenaEitens = new ArrayList<>();
+           String cenaJson;
+           String itemJson;
+           cenaEitens.add(cenaJson = GSON.toJson(cena));
+           cenaEitens.add(itemJson = GSON.toJson(item));
+
+           Spark.get("/", (req, res) -> cenaEitens);
+
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
