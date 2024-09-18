ceb76e7937a45e7c97f9792d05ee2dd7c9951de8
Azvedo-06 <azinho006@gmail.com>
2024-09-18 12:35:45 -0300

    alteração no php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
-if ($_GET['comando'] && $_GET['save']) {
-    $comando = rawurlencode($_GET['comando']);
-    $save = rawurlencode($_GET['save']);
-    $conteudo = file_get_contents("http://localhost:4567/{$comando}/{$save}");
-} else if(isset($_GET['comando'])) {
+
+if ($_GET['comando']) {
     $comando = rawurlencode($_GET['comando']);
     $conteudo = file_get_contents("http://localhost:4567/{$comando}");
-} else {
+}
+else if($conteudo = " " || $conteudo = null) {
+    echo "Comando não encontrado ou invalido..";
+}
+else {
     $conteudo = file_get_contents("http://localhost:4567/");
 }
 
